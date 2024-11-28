04306208c622e6bcf3db516388605ac6177bd144
Rostislav <91129946+homeboyinlove@users.noreply.github.com>
2024-11-28 21:08:19 +0300

    Create lab7.py

---
+++ lab7.py
@@ -0,0 +1,29 @@
+import turtle
+
+
+def draw_koch_segment(t, ln):
+    if ln > 6:
+        ln3 = ln // 3
+        draw_koch_segment(t, ln3)
+        t.left(60)
+        draw_koch_segment(t, ln3)
+        t.right(120)
+        draw_koch_segment(t, ln3)
+        t.left(60)
+        draw_koch_segment(t, ln3)
+    else:
+        t.fd(ln)
+        t.left(60)
+        t.fd(ln)
+        t.right(120)
+        t.fd(ln)
+        t.left(60)
+        t.fd(ln)
+
+
+t = turtle.Turtle()
+ln = 200
+t.ht()
+t.speed(200)
+draw_koch_segment(t, ln)
+turtle.done()
